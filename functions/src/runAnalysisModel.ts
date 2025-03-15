11c3fbfaa088ddb123bc745c824a4422996f074e
Benjamin Rose <55436674+benjaminharringtonrose@users.noreply.github.com>
2025-03-15 15:21:25 -0400

    cleanup

--- functions/src/runAnalysisModel.ts
+++ functions/src/runAnalysisModel.ts
@@ -3,13 +3,11 @@ import { onSchedule } from "firebase-functions/v2/scheduler";
 import { ANALYSIS_SCHEDULE } from "./constants";
 import { calculateSellDecision } from "./calculations/calculateSellDecision";
 import { sendSmsNotification } from "./notifications/sendSmsNotification";
-import { trainSellModel } from "./machineLearning/trainSellModel";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 export const runAnalysisModel = onSchedule(ANALYSIS_SCHEDULE, async () => {
-  await trainSellModel();
   const value = await calculateSellDecision("cardano");
   await sendSmsNotification(JSON.stringify(value));
   console.log(value);

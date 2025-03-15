e8860a180bc15fa66da8e1066022dbb218c74a69
Benjamin Rose <55436674+benjaminharringtonrose@users.noreply.github.com>
2025-03-15 14:02:11 -0400

    cleanup

--- functions/src/constants.ts
+++ functions/src/constants.ts
@@ -5,7 +5,7 @@ export const COINGECKO_API_URL = "https://api.coingecko.com/api/v3";
 
 export const PRICES = [0.8, 0.9, 1.0, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5];
 export const PRICE_CHECK_SCHEDULE = `*/1 * * * *`; // every 1 min
-export const ANALYSIS_SCHEDULE = `3 14 * * *`;
+export const ANALYSIS_SCHEDULE = `3 14 * * *`; // every day at 2:03 PM ET
 export const NOTIFICATION_COOLDOWN = 30 * 60 * 1000; // 30 mins
 
 export const RUNNING_SCHEDULE_CHECK_MESSAGE =

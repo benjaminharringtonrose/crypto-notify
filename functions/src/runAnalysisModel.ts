3f02a7d785320ff4b50e2e9a7bf1619d21a15fce
Benjamin Rose <55436674+benjaminharringtonrose@users.noreply.github.com>
2025-03-15 14:14:08 -0400

    cleanup

--- functions/src/runAnalysisModel.ts
+++ functions/src/runAnalysisModel.ts
@@ -2,9 +2,11 @@ import { onSchedule } from "firebase-functions/v2/scheduler";
 import { ANALYSIS_SCHEDULE } from "./constants";
 import { calculateSellDecision } from "./calculations/calculateSellDecision";
 import { sendSmsNotification } from "./notifications/sendSmsNotification";
+import { trainSellModel } from "./machineLearning/trainSellModel";
 
 export const runAnalysisModel = onSchedule(ANALYSIS_SCHEDULE, async () => {
+  await trainSellModel();
   const value = await calculateSellDecision("cardano");
-  console.log(value);
   await sendSmsNotification(JSON.stringify(value));
+  console.log(value);
 });

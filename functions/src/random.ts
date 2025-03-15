11bcac6c5c01bfe352fc4b39cb25e69cfc5a46c6
Benjamin Rose <55436674+benjaminharringtonrose@users.noreply.github.com>
2025-03-15 15:10:03 -0400

    cleanup

--- functions/src/random.ts
+++
@@ -1,4 +0,0 @@
-import { onSchedule } from "firebase-functions/v2/scheduler";
-import { ANALYSIS_SCHEDULE } from "./constants";
-
-export const random = onSchedule(ANALYSIS_SCHEDULE, async () => {});

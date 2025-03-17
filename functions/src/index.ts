4ce489d2036f765608ad9c391bdcc8d522289ef2
Benjamin Rose <55436674+benjaminharringtonrose@users.noreply.github.com>
2025-03-17 18:52:08 -0400

    cleanup

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,7 @@ import * as admin from "firebase-admin";
 
 admin.initializeApp();
 
-export { schedulePriceCheckADA } from "./scheduled/schedulePriceCheckADA";
 export { runTradeModel } from "./scheduled/runTradeModel";
-// export { runTradeModelDaily } from "./scheduled/runTradeModelDaily";
 export { receiveSMS } from "./notifications/recieveSMS";
+// export { schedulePriceCheckADA } from "./scheduled/schedulePriceCheckADA";
+// export { runTradeModelDaily } from "./scheduled/runTradeModelDaily";

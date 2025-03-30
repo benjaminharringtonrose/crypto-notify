c48a3b4767f9b7258fab0387854371b6b06c1f6d
Benjamin Rose <55436674+benjaminharringtonrose@users.noreply.github.com>
2025-03-30 10:34:03 -0400

    add log

--- functions/src/pubsub/runModelTrainingADA.ts
+++ functions/src/pubsub/runModelTrainingADA.ts
@@ -3,6 +3,7 @@ import { TradeModelTrainer } from "../cardano/TradeModelTrainer";
 import { TRAINING_CONFIG } from "../constants";
 
 export const runModelTrainingADA = onSchedule(TRAINING_CONFIG, async () => {
+  console.log("Training started...");
   const trainer = new TradeModelTrainer();
   await trainer.train();
 });
